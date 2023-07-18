ff19a4b051d331934c5406cfb1a87fd8ed14a69b
thompa21 <tholind@kth.se>
2023-07-18 10:45:06 +0200

    offerrequest

--- Models.js
+++ Models.js
@@ -16,8 +16,8 @@ const readLanguages = () => {
 
 const readHowManySpeak = (langid) => {
   return new Promise(function (resolve, reject) {
-      const sql = `SELECT COUNT(id) FROM tll_users WHERE (lang_have_1 = '$langid' OR lang_have_1 = '$langid') AND last_login > current_date - interval 6 month `;
-      database.db.query(database.mysql.format(sql,[]),(err, result) => {
+      const sql = `SELECT COUNT(id) FROM tll_users WHERE (lang_have_1 = ? OR lang_have_1 = ?) AND last_login > current_date - interval 6 month `;
+      database.db.query(database.mysql.format(sql,[langid,langid]),(err, result) => {
           if(err) {
               console.error(err);
               reject(err.message)
@@ -29,8 +29,8 @@ const readHowManySpeak = (langid) => {
 
 const readHowManyWant = (langid) => {
   return new Promise(function (resolve, reject) {
-      const sql = `SELECT COUNT(id) FROM tll_users WHERE (lang_want_1 = '$langid' OR lang_want_2 = '$langid') AND last_login > current_date - interval 6 month `;
-      database.db.query(database.mysql.format(sql,[]),(err, result) => {
+      const sql = `SELECT COUNT(id) FROM tll_users WHERE (lang_want_1 = ? OR lang_want_2 = ?) AND last_login > current_date - interval 6 month `;
+      database.db.query(database.mysql.format(sql,[langid,langid]),(err, result) => {
           if(err) {
               console.error(err);
               reject(err.message)
